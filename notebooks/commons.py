a76987fc78d5f828470aa0731e55099a2e8e5d25
DiTo97 <fede97.minutoli@gmail.com>
2021-11-27 16:56:14 +0100

    Experiments with RS and PO data

--- notebooks/commons.py
+++ notebooks/commons.py
@@ -8,13 +8,33 @@ DIR_root = Path(__file__).absolute().parents[1]
 DIR_data = DIR_root / 'data'
 
 DIR_raw  = DIR_data / 'raw'
-DIR_processed = DIR_data / 'processed'
+DIR_pro  = DIR_data / 'processed'
 
-DIR_raw_pace = DIR_raw / 'Pace'
-DIR_raw_players = DIR_raw / 'Players'
-DIR_raw_teams = DIR_raw / 'Teams'
+# Playoffs
+DIR_raw_po = DIR_raw / 'PO'
+DIR_pro_po = DIR_pro / 'PO'
 
-DIR_processed_league = DIR_processed / 'League'
+DIR_raw_po_pace    = DIR_raw_po / 'pace'
+DIR_raw_po_players = DIR_raw_po / 'players'
+DIR_raw_po_teams   = DIR_raw_po / 'teams'
+
+DIR_pro_po_teams   = DIR_pro_po / 'teams'
+DIR_pro_po_players = DIR_pro_po / 'players'
+DIR_pro_po_league  = DIR_pro_po / 'league'
+
+# Regular season
+DIR_raw_rs = DIR_raw / 'RS'
+DIR_pro_rs = DIR_pro / 'RS'
+
+DIR_raw_rs_pace    = DIR_raw_rs / 'pace'
+DIR_raw_rs_players = DIR_raw_rs / 'players'
+DIR_raw_rs_teams   = DIR_raw_rs / 'teams'
+
+DIR_pro_rs_teams   = DIR_pro_rs / 'teams'
+DIR_pro_rs_players = DIR_pro_rs / 'players'
+DIR_pro_rs_league  = DIR_pro_rs / 'league'
+
+GP_min = 3
 
 YEAR_start = 1996
 YEAR_end   = 2020
@@ -50,6 +70,20 @@ def year2rangestr(year: int) -> str:
 
 def year2filename(year: int, ext: str = 'csv') -> str:
     year_range = year2rangestr(year)
+    
+    return '{}.{}'.format(year_range,
+                          ext)
+
+
+def total_year2rangestr(year1: int, year2: int) -> str:
+    year2_suffix = str(year2)[2:]
+    
+    return '{}-{}'.format(year1, year2_suffix)
+
+
+def total_year2filename(year1: int, year2: int,
+                        ext: str = 'csv') -> str:
+    year_range = total_year2rangestr(year1, year2)
     
     return '{}.{}'.format(year_range,
                           ext)
